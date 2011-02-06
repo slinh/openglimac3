24a2a97452eda627638de36b5150997a4187d73c
camille.bossoutrot <camille.bossoutrot@cce0daa8-7986-442a-893b-aa0e03d8582c>
2011-02-06 17:48:52 +0000

    correction env cube map

--- Sources/main.hpp
+++ Sources/main.hpp
@@ -38,7 +38,7 @@ static unsigned int windowheight = 600;
 GLenum err;
 
 // Modele de la camera
-static float position[] = {-1.0f,0.5f,9.0f};
+static float position[] = {-1.0f,0.5f,0.0f};
 static float aim[] = {0.0f,0.0f,-1.0f};
 static float direction[] = {0.0f,0.0f,-1.0f};
 static float up[] = {0.0f, 1.0f, 0.0f};
